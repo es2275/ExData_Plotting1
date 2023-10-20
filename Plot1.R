afa8c0d0cbf6fbcab074d486b9b2d77292c41b83
es2275 <148571177+es2275@users.noreply.github.com>
2023-10-20 16:02:30 -0700

    Update Plot1.R

--- Plot1.R
+++ Plot1.R
@@ -12,3 +12,4 @@ plot1 <- hist(plot1data$Global_active_power, main="Global Active Power",
      xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red")
 
 png("plot1.png", width=480, height=480)
+devoff()
